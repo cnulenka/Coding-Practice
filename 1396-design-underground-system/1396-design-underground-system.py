bd44bf49c163daaeb50decdead0f344015c46c22
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-02-13 20:57:25 +0530

    Time: 308 ms (55.42%), Space: 23.6 MB (97.82%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -2,35 +2,28 @@ class UndergroundSystem:
 
     def __init__(self):
         self.customerTravelling = dict()
-        self.tourAvg = defaultdict(float)
+        self.tourAvg = defaultdict(lambda : [0, 0])
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         
-        if id not in self.customerTravelling:
-            self.customerTravelling[id] = (stationName, t)
+        self.customerTravelling[id] = [stationName, t]
 
     def checkOut(self, id: int, endStation: str, endTime: int) -> None:
         
-        if id in self.customerTravelling:
-            startStation, startTime = self.customerTravelling[id]
+        startStation, startTime = self.customerTravelling[id]
             
-            # compute tour average
-            tourId = startStation + '_' + endStation
-            
-            if tourId in self.tourAvg:
-                prevAvg, numTravelers = self.tourAvg[tourId]
-                newAvg = ((prevAvg*numTravelers) + (endTime-startTime))/(numTravelers+1)
-                self.tourAvg[tourId] = (newAvg, numTravelers+1)
-            else:
-                self.tourAvg[tourId] = (endTime-startTime, 1)
-            
-            # remove passenger
-            self.customerTravelling.pop(id)
+        # compute tour average
+        tourId = startStation + '_' + endStation
+        self.tourAvg[tourId][0] += endTime-startTime
+        self.tourAvg[tourId][1] += 1
+
+        # remove passenger to save space
+        self.customerTravelling.pop(id)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         tourId = startStation + '_' + endStation
-        Avg, numTravelers = self.tourAvg[tourId]
-        return Avg
+        routeSum, numTravelers = self.tourAvg[tourId]
+        return routeSum/numTravelers
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
