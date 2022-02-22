019f0769b965a4c4ef0b2f2ad9d5f09f07fe5bc1
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-02-22 22:09:29 +0530

    Time: 48 ms (62.89%), Space: 14.3 MB (59.81%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        
+        visited = [False for _ in range(len(s))]
+        q = collections.deque([0])
+        #visited[0] = True
+        
+        while q:
+            start = q.popleft()
+            if visited[start]:
+                continue
+            
+            
+            for end in range(start+1, len(s)+1):
+                if s[start:end] in wordDict:
+                    print(start, end)
+                    #if not visited[end]:
+                    q.append(end)
+                    
+                    if end == len(s):
+                        return True
+                    
+            visited[start] = True
+            
+        
+        return False
