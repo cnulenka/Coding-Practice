59098cb5c129f94d75a7379e5e4f31f6a55b5e47
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-02-28 21:36:12 +0530

    Time: 108 ms (77.28%), Space: 17.3 MB (26.46%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        
+        def isCyclic(currCourse):
+            nonlocal courseDict, checked, path
+            
+            if checked[currCourse]:
+                return False
+            
+            if path[currCourse]:
+                return True
+            
+            path[currCourse] = True
+            
+            for neigh in courseDict[currCourse]:
+                if isCyclic(neigh):
+                    return True
+            
+            path[currCourse] = False
+            checked[currCourse] = True
+            
+            return False
+        
+        from collections import defaultdict
+        courseDict = defaultdict(list)
+
+        for relation in prerequisites:
+            courseDict[relation[0]].append(relation[1])
+            
+        checked = [False] * numCourses
+        path = [False] * numCourses
+        
+        for i in range(numCourses):
+            if isCyclic(i):
+                return False
+        
+        return True
