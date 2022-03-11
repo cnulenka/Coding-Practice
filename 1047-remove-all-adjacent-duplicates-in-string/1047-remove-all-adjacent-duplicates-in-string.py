2b7d6bad8e6d79b224744a9742c37117d2043320
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-03-11 21:48:19 +0530

    Time: 134 ms (36.79%), Space: 14.9 MB (39.11%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -9,20 +9,8 @@ class Solution:
             if st and st[-1] == s[i]:
                 st.pop()
                 i=i+1
-                #print(i, s[i], st, flag)
-                #print("hola1")
-                #while  i < len(s) and st[-1] == s[i]:
-                #    flag = True
-                #    i+=1
-                #if flag:
-                #    st.pop()
-                #    flag = False
-                #print(i, s[i], st, flag)
             else:
-                #print(i, s[i], st, flag)
-                #print("hola2")
                 st.append(s[i])
-                #print(i, s[i], st, flag)
                 i+=1
         
         return "".join(st)
