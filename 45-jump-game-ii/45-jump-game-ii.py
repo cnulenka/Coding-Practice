809b208bc3aae5f0c9da98bf5adeaeae3c080ef9
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-06-19 02:42:17 +0530

    Time: 234 ms (48.96%), Space: 15.2 MB (23.76%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         jumps = 0
-        curr_search_space = 0
-        farthest = 0
+        curr_max_reach = 0
+        max_reach = 0
         
         for i in range(len(nums)-1):
-            farthest = max(farthest, i+nums[i])
+            max_reach = max(max_reach, i+nums[i])
             
-            if i == curr_search_space:
+            if i == curr_max_reach:
                 jumps += 1
-                curr_search_space = farthest
+                curr_max_reach = max_reach
         
         return jumps
