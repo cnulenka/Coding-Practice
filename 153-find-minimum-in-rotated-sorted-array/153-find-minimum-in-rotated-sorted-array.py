7ddeb0f47466b7f8a1d8cd6bfe9ee2905b840a93
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-07-29 00:54:20 +0530

    Time: 72 ms (32.38%), Space: 14.3 MB (23.33%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -7,8 +7,11 @@ class Solution:
         if nums[0] < nums[-1]:
             return nums[0]
         
+        # take care of end conditions b4 hand
         # if nums[-1] < nums[-2]:
         #     return nums[-1]
+        
+        # look at your cpp solution
         
         l = 0
         r = len(nums) - 1
@@ -16,17 +19,20 @@ class Solution:
         while(l <= r):
             mid = l + (r - l)//2
             # print(l, mid, r)
-            # if nums[mid-1] > nums[mid] and nums[mid] < nums[mid+1]:
-            #     return nums[mid]
-            
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            
-            if nums[mid - 1] > nums[mid]:
+            if (mid == 0 or nums[mid-1] > nums[mid]) and (mid == len(nums) -1 or nums[mid] < nums[mid+1]):
                 return nums[mid]
+            
+            # trick, only focus on two numbers
+            # order is important in java
+#             if nums[mid] > nums[mid + 1]:
+#                 return nums[mid + 1]
+            
+#             if nums[mid - 1] > nums[mid]:
+#                 return nums[mid]
         
         
-            if nums[mid] > nums[0]:
+            # if nums[mid] > nums[0]: # what if mid is 0 itself
+            if nums[mid] >= nums[0] and nums[mid] >= nums[-1]:
                 l = mid+1
             else:
                 r = r - 1
