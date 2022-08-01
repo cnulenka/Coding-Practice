58702421b68e35ce12e295bd390bfbded982260b
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-08-02 00:35:58 +0530

    Time: 515 ms (84.78%), Space: 15.4 MB (75.42%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+        
+        while low < high: # <= leads to TLE # tricky
+            speed = low + (high-low)//2
+            
+            hours = 0
+            
+            for pile in piles:
+                hours += math.ceil(pile/speed)
+            
+            if hours > h:
+                # speed is too low, increase speed
+                low = speed + 1
+            else:
+                # speed is too high, decrease speed
+                high = speed
+        
+        
+        return high
+        
+        
