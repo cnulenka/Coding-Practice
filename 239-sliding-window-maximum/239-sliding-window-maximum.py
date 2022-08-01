1d153c674e2a6ff050896052ba45dd92043eb2d1
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-08-02 01:26:11 +0530

    Time: 2639 ms (60.14%), Space: 30.1 MB (34.81%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -6,20 +6,14 @@ class Solution:
         
         for i in range(n):
             #print(q, i, k)
-            while q:
-                if q[0] + k - 1 < i:
+            while q and q[0] <= i - k:
                     q.popleft()
-                else:
-                    break
-            #print(q, i, k, "mid")
+
             if not q:
                 q.append(i)
             else:
-                while q:
-                    if nums[i] >= nums[q[-1]]:
+                while q and nums[i] >= nums[q[-1]]:
                         q.pop()
-                    else:
-                        break
                 q.append(i)
             #print(q, i, k, "end")
             if i >= k - 1:
