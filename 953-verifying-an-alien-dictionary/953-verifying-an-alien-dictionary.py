0362450f66282912de0dc92b60ce9270768017e5
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-08-14 02:03:54 +0530

    Time: 50 ms (68.20%), Space: 13.8 MB (84.16%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = {}
+        
+        for i,c in enumerate(order):
+            order_map[c] = i
+            
+        for i in range(len(words) - 1):
+            
+            # loop over ith word, if it has more chars we can check
+            for j in range(len(words[i])):
+                
+                if j >= len(words[i+1]):
+                    return False
+                
+                if words[i][j] != words[i+1][j]: # only check letter that are different
+                    if order_map[words[i][j]] > order_map[words[i+1][j]]:
+                        return False
+                    
+                    # we dont need to examine rest 
+                    # tricky
+                    break
+        
+        return True
