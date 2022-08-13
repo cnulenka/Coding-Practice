ebd80d03da058c007f490c0804217dbf645e0e38
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-08-13 19:37:55 +0530

    Time: 1281 ms (18.01%), Space: 139.6 MB (16.50%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,27 +1,23 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        def solve(m, n):
-            nonlocal hash_dict
-            #print(hash_dict)
-            
-            if (m,n) in hash_dict:
-                return hash_dict[(m,n)]
+        dp = {}
+        def lcs(m, n):
             
             if m == 0 or n == 0:
-                hash_dict[(m, n)] = 0
                 return 0
             
+            if (m,n) in dp:
+                return dp[(m,n)]
+            
             if text1[m-1] == text2[n-1]:
-                return 1 + solve(m-1, n-1)
+                dp[(m,n)] = 1 + lcs(m-1, n-1)
+            else:
+                dp[(m,n)] = max(lcs(m-1, n), lcs(m, n-1))
             
-            res1 = solve(m-1, n)
-            hash_dict[(m-1, n)] = res1
+            return dp[(m,n)]
             
-            res2 = solve(m, n-1)
-            hash_dict[(m, n-1)] = res2
-            
-            return max(res1, res2)
+        m = len(text1)
+        n = len(text2)
         
-        hash_dict = {}
-        return solve(len(text1), len(text2))
+        return lcs(m, n)
