c2b3e43317e0e172d3ed5d714b1d80d24b8e7d22
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-08-16 01:38:23 +0530

    Time: 2006 ms (5.05%), Space: 33.2 MB (69.72%) - LeetHub

---
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+        summ = 0
+        # put a rem 0 case 
+        # tricky
+        dp = {0:-1}
+        
+        for i,n in enumerate(nums):
+            
+            summ = (summ + n) % k
+                
+            if summ not in dp:
+                dp[summ] = i
+            else:
+                if i - dp[summ] >= 2:
+                    return True
+        
+        
+        return False
+            
+            
+            
+            
+            
+        
+        
