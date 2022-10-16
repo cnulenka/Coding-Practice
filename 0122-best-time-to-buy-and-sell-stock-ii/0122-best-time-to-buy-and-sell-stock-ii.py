df590ca97ae4cc4bf28fe4c02107e6225719723a
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-10-16 21:31:06 +0530

    Time: 57 ms (98.84%), Space: 15.1 MB (68.99%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,31 +4,10 @@ class Solution:
         The key point is we need to consider every peak immediately following a valley to maximize the profit. In case we skip one of the peaks (trying to obtain more profit), we will end up losing the profit over one of the transactions leading to an overall lesser profit.
         '''
         
-        n = len(prices)
-        dp = {}
-        
-        def get_profit(ind, buy):
-            
-            if ind == n:
-                return 0
-            
-            if (ind, buy) in dp:
-                return dp[(ind, buy)]
-            
-            profit = 0
-            if buy:
-                profit = max(
-                    -prices[ind] + get_profit(ind+1, 1-buy),
-                    get_profit(ind+1, buy)
-                )
-            else:
-                profit = max(
-                    prices[ind] + get_profit(ind+1, 1-buy),
-                    get_profit(ind+1, buy)
-                )
-            
-            dp[(ind, buy)] = profit
-            return dp[(ind, buy)]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+                i+=1
                 
-        
-        return get_profit(0, 1)
+        return profit
