ade3847f480578e7958e85723cecc538cc8af585
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-10-16 21:33:28 +0530

    Time: 117 ms (57.14%), Space: 15.3 MB (25.60%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -8,6 +8,5 @@ class Solution:
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
-                i+=1
                 
         return profit
