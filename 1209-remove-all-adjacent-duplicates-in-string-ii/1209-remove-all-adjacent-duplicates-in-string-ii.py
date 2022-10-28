c53b21c0a8887a16c8a64fd5415d9c9dd66395a3
Shakti Prasad Lenka <sinulenka001@gmail.com>
2022-10-29 00:50:52 +0530

    Time: 495 ms (12.43%), Space: 15.9 MB (88.94%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,33 +1,29 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        char_stk = ['#']
-        num_stk = [0]
+        char_stk = [s[0]]
+        num_stk = [1]
         
-        for c in s:
+        for i in range(1, len(s)):
             
             # check if last k-1 letters
             # are same as curr char
             
-            # print(char_stk, num_stk)
+            # print(char_stk, num_stk, i, s[i])
+
+            if char_stk and char_stk[-1] == s[i]:
                 
-            if char_stk[-1] == c and num_stk[-1] == k - 1:
+                if num_stk[-1] == k - 1:
 
-                    pop_count = num_stk[-1]
-
-                    while pop_count:
+                    while num_stk[-1]:
                         char_stk.pop()
-                        num_stk.pop()
-                        pop_count -= 1
+                        num_stk[-1] -= 1
 
-                    continue
-                
-            
-            if char_stk[-1] == c:
-                num_stk.append(num_stk[-1] + 1)
+                    num_stk.pop()
+                else:
+                    num_stk[-1] += 1
+                    char_stk.append(s[i])
             else:
                 num_stk.append(1)
-
-            char_stk.append(c)
+                char_stk.append(s[i])
         
-        char_stk = char_stk[1:]
         return "".join(char_stk)
