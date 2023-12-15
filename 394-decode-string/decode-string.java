53de6a5fdad7e4babf597a6f11fb043bb042d470
Shakti Prasad Lenka <sinulenka001@gmail.com>
2023-12-16 01:59:53 +0530

    Time: 5 ms (20.03%) | Memory: 41 MB (15.26%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        int currNum = 0;
+        StringBuilder currStr = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            System.out.println(c);
+            System.out.println(currStr.toString());
+            if( Character.isDigit(c)){
+                currNum = currNum*10 + c - '0';
+            } else if ( c == '['){
+                numStack.push(currNum);
+                strStack.push(currStr.toString());
+                currNum = 0;
+                currStr = new StringBuilder();
+            } else if (c == ']'){
+                Integer lastNum = numStack.pop();
+                StringBuilder lastStr = new StringBuilder(strStack.pop());
+                
+                for(int j = 0; j< lastNum; j++){
+                    lastStr.append(currStr.toString());
+                }
+                currStr = lastStr;
+            } else {
+                currStr.append(c);
+            }
+        }
+
+        return currStr.toString();
+    }
+}
