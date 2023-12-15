b9a53fd0913ea9bd0c3537c555924634ef6b6e05
Shakti Prasad Lenka <sinulenka001@gmail.com>
2023-12-16 02:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (47.96%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -5,8 +5,6 @@ class Solution {
         int currNum = 0;
         StringBuilder currStr = new StringBuilder();
         for(char c : s.toCharArray()) {
-            System.out.println(c);
-            System.out.println(currStr.toString());
             if( Character.isDigit(c)){
                 currNum = currNum*10 + c - '0';
             } else if ( c == '['){
