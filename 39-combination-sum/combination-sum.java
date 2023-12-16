2bfc1ec6dafdabab9c3449e82f6fd4c159783b9a
Shakti Prasad Lenka <sinulenka001@gmail.com>
2023-12-16 19:02:17 +0530

    Time: 2 ms (75.88%) | Memory: 43.8 MB (58.87%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,27 @@
 class Solution {
-    List<List<Integer>> res;
-    private void computeCombSum(int currIndex, int target, int[] candidates, List<Integer> currList) {
 
-        if (currIndex == candidates.length || target<0) {
+    private void solve(int index, int[] candidates, int target, List<Integer> currList, List<List<Integer>> res) {
+
+        if(target < 0){
             return;
         }
 
-        if(target == 0) {
+        if(target == 0){
             res.add(new ArrayList<>(currList));
             return;
         }
 
-        currList.add(candidates[currIndex]);
-        computeCombSum(currIndex, target - candidates[currIndex], candidates, currList); // inlcude
-        currList.remove(currList.size()-1);
-        computeCombSum(currIndex+1, target, candidates, currList); // exclude
+        for(int i = index; i < candidates.length; i++){
+            currList.add(candidates[i]);
+            solve(i, candidates, target - candidates[i], currList, res);
+            currList.remove(currList.size() - 1);
+        }
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        res = new ArrayList<>();
-        computeCombSum(0, target, candidates, new ArrayList<>());
-        return res;
+        
+        List<List<Integer>> ans = new ArrayList();
+        solve(0, candidates, target, new ArrayList(), ans);
+        return ans;
     }
 }
