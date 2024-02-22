49593a514337f884bc069f4292643484cd3f6ced
Shakti Prasad Lenka <sinulenka001@gmail.com>
2024-02-22 09:42:14 +0530

    Time: 98 ms (20.34%) | Memory: 56.8 MB (35.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,40 +1,51 @@
 class Solution {
 
-    Boolean[][] memo;
+    int[][] dp;
 
-    private boolean subSetSum(int targetSum, int currIndex, int[] nums){
+    private boolean solve(int[] nums, int ind, int targetSum) {
 
-        if(nums.length == currIndex ||  targetSum < 0) {
-            return false;
-        }
-
-        if(targetSum == 0){
+        if(targetSum == 0) {
             return true;
         }
 
-        if (memo[targetSum][currIndex] != null)
-            return memo[targetSum][currIndex];
+        if(ind == nums.length || targetSum < 0) {
+            return false;
+        }
 
-        boolean include = subSetSum(targetSum - nums[currIndex], currIndex+1, nums);
-        boolean exclude = subSetSum(targetSum, currIndex+1, nums);
+        if(dp[ind][targetSum] != -1){
+            return dp[ind][targetSum] == 1;
+        }
 
-        boolean res =  include || exclude;
-        memo[targetSum][currIndex] = res;
-        return res;
+        // include
+        boolean include = solve(nums, ind + 1, targetSum - nums[ind]);
+        // exclude
+        boolean exclude = solve(nums, ind+1, targetSum);
+
+        if(include || exclude){
+            dp[ind][targetSum] = 1;
+        } else {
+            dp[ind][targetSum] = 0;
+        }
+        
+        return dp[ind][targetSum] == 1;
     }
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int n: nums){
-            sum+=n;
+        for(int num : nums){
+            sum+=num;
         }
 
-        if(sum%2 != 0){
+        if(sum%2 != 0) {
             return false;
         }
 
-        int targetSum = sum/2,  n = nums.length;
-        memo = new Boolean[targetSum + 1][n + 1];
-        return subSetSum(targetSum, 0, nums);
+        int targetSum = sum/2;
+        dp = new int[nums.length][targetSum+1];
+        for(int i = 0 ; i < nums.length ; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(nums, 0, targetSum);
     }
 }
