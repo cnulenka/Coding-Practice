ccc34b3c069da4e68c982da63cac7f33542421de
Shakti Prasad Lenka <sinulenka001@gmail.com>
2024-02-24 11:24:49 +0530

    Time: 24 ms (85.35%) | Memory: 62.9 MB (61.15%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumCandies (int[] candies, long k) {
+        int max = 0;
+        for (int i : candies) {
+            max = (max > i) ? max : i;
+        }
+        int start = 1, end = max, mid = 0, res = 0;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (isValid(candies, k, mid)) {
+                res = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return res;
+    }
+    private boolean isValid(int[] candies, long k, int mid) {
+        long totalPiles = 0;
+        for (int i : candies) {
+            int toAdd = i / mid;
+            totalPiles += toAdd;
+        }
+        return totalPiles >= k;
+    }
+
+}
