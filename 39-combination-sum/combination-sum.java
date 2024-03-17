662cb943796d6a474f9f9321cb38fd4100c5bd9e
Shakti Prasad Lenka <sinulenka001@gmail.com>
2024-03-17 15:09:19 +0530

    Time: 2 ms (84.08%) | Memory: 44.4 MB (67.43%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -24,4 +24,25 @@ class Solution {
         solve(0, candidates, target, new ArrayList(), ans);
         return ans;
     }
+    /*
+    Let N be the number of candidates, T be the target value, and M be the minimal value among the candidates.
+
+    Time Complexity: O(N^(T/M+1))
+
+        As we illustrated before, the execution of the backtracking is unfolded as a DFS traversal in a n-ary tree.
+        The total number of steps during the backtracking would be the number of nodes in the tree.
+
+        At each node, it takes a constant time to process, except the leaf nodes which could take a linear time 
+        to make a copy of combination. 
+        So we can say that the time complexity is linear to the number of nodes of the execution tree.
+
+        Here we provide a loose upper bound on the number of nodes.
+
+            First of all, the fan-out of each node would be bounded to N, i.e. the total number of candidates.
+
+            The maximal depth of the tree, would be T/M​, where we keep on adding the smallest 
+            element to the combination.
+
+            As we know, the maximal number of nodes in N-ary tree of T/M​ height would be O(N^(T/M+1)).
+*/
 }
